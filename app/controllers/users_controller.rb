0c7f0650bd2fee5b712f90f4d3b10dd5c9bf3d46
Geoff <geoff.register@gmail.com>
2018-09-11 15:46:19 -0400

    many changes - little working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,24 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def show
-  end
-
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render 'new'
+    end
   end
 
   def destroy
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-      root 'questions#index'
-        resources :users do
+      resources :users, only: [:new, :create] do
+        root 'questions#index'
           resources :questions do
             resources :answers do
               resource :session, only: :create
         end
       end
     end
-
+    root 'questions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
