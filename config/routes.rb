80f6a6dac88c4829e594e9e0111f4714d9d18f60
Geoff <geoff.register@gmail.com>
2018-09-11 22:56:31 -0400

    Started the sessions controller and view form

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
         root 'questions#index'
           resources :questions do
             resources :answers do
-              resource :session, only: :create
+              
         end
       end
     end
     root 'questions#index'
+    resource :session, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
