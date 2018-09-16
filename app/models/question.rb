58c981e703d417eca0466abe145825ca1a9255ee
Geoff <geoff.register@gmail.com>
2018-09-16 18:58:51 -0400

    some minor changes to nothing

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
     paginates_per 5
     belongs_to :user
-    has_many :answers, as: :answerable
+    has_many :answers, as: :answerable, dependent: :destroy
     validates :title, :body, :user_id, presence: true
 end
